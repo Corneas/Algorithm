7cdedf061f05f04aa3f8044d2755714157e29746
냠냠 <lanilani7661@gmail.com>
2022-05-29 22:52:20 +0900

    정리

--- 86491(not done).cpp
+++ 86491(not done).cpp
@@ -4,13 +4,9 @@
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
-    int answer = (sizes[0][0] * sizes[0][1]);
+    int answer = 0;
 
-    for (int i = 0; i < sizes.size(); i++) {
-        for (int j = 0; j < sizes.size() - 1; j++) {
-            answer = sizes[i][j] * sizes[i][j+1];
-        }
-    }
+   
 
     return answer;
 }
