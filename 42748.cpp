6a42bb47ccb89385408093170e59b1248285adc8
냠냠 <lanilani7661@gmail.com>
2022-09-12 23:35:49 +0900

    trying 42748

---
+++ 42748.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> v1;
+    int first = 0;
+    int end = 0;
+
+    // commands[0][0] ~ command[0][size]
+    v1.clear();
+
+    for (int i = 0; i < commands.size(); ++i) {
+        first = commands[i][0];
+        end = commands[i][1];
+
+        for (int j = first; j <= end; ++j) {
+            v1.push_back(array[j]);
+        }
+
+        sort(v1.begin(), v1.end());
+        answer.push_back(v1[commands[i][2]]);
+        v1.clear();
+    }
+
+    return answer;
+}
