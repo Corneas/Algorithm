c508195b6d36afba5ea88c3113cd9eae586d8af0
냠냠 <lanilani7661@gmail.com>
2022-09-13 10:28:47 +0900

    solving 42748

--- 42748.cpp
+++ 42748.cpp
@@ -9,22 +9,22 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> v1;
     int first = 0;
     int end = 0;
-
-    // commands[0][0] ~ command[0][size]
+    
     v1.clear();
 
     for (int i = 0; i < commands.size(); ++i) {
-        first = commands[i][0];
-        end = commands[i][1];
+        first = commands[i][0] - 1;
+        end = commands[i][1] - 1;
 
         for (int j = first; j <= end; ++j) {
             v1.push_back(array[j]);
         }
 
         sort(v1.begin(), v1.end());
-        answer.push_back(v1[commands[i][2]]);
+        answer.push_back(v1[commands[i][2] - 1]);
         v1.clear();
     }
 
     return answer;
 }
+
