3abf5ca7f58bb26ed699b9329a5020a3d3fed769
냠냠 <lanilani7661@gmail.com>
2022-09-28 16:11:58 +0900

    solving 1037

--- 1037.cpp
+++ 1037.cpp
@@ -1,1 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	vector<int> v;
+
+	int n = 0;
+	int a = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		cin >> a;
+
+		v.push_back(a);
+	}
+
+	sort(v.begin(), v.end());
+
+	n = v.front() * v.back();
+
+	cout << n;
+}
