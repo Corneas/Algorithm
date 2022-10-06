ec727c63ee79038be82df2d831d8fdcec4bdedef
냠냠 <lanilani7661@gmail.com>
2022-10-06 23:38:22 +0900

    [Programmers] solving 70129

--- 70129.cpp
+++ 70129.cpp
@@ -1,29 +1,45 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(string s) {
     vector<int> answer;
-
-    int bCount = 0;     // 이진변환 개수
-    int zCount = 0;     // 삭제할 0의 개수
+    vector<int> v;
+    int totalZero = 0;
     int length = 0;
+    int change = 0;
 
-    for (int i = 0; i < s.length(); ++i) {
-        if (s[i] == '0') {
-            s.erase(s.begin() + i, s.begin() + i + 1);
-            zCount++;
-            --i;
+    while (true) {
+        if (s.length() == 1) {
+            break;
         }
-        length - s.length();
+        v.clear();
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '0') {
+                totalZero++;
+                s.erase(s.begin() + i, s.begin() + i + 1);
+                i--;
+            }
+        }
+
+        length = s.length();
 
         while (length > 0) {
-
+            v.push_back(length % 2);
+            length /= 2;
         }
-
+        s = "";
+        for (int i = v.size() - 1; i >= 0; --i) {
+            s += to_string(v[i]);
+        }
+        change++;
     }
+
+    answer.push_back(change);
+    answer.push_back(totalZero);
 
     return answer;
 }
