a5fece89009ce776139712be3cd5eef85eaf02a7
냠냠 <lanilani7661@gmail.com>
2022-10-16 22:54:51 +0900

    [BaekJoon] trying 1015

---
+++ B_1015.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	int N[1001];
+
+	// 공식 : 
+	// while(오른쪽 사이트 개수) - (왼쪽 사이트 개수 - 1) == 1)
+	// N[0] + N[1] + ... N[(오른쪽 사이트 개수) - (왼쪽 사이트 개수 - 1)];
+	// ?
+}
