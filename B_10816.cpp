def161085e409d890bd676d5e06573c9aebe89ed
Minkyu Kwak <lanilani7661@gmail.com>
2022-11-15 10:56:46 +0900

    [BaekJoon] solved 10816 by binary search

--- B_10816.cpp
+++ B_10816.cpp
@@ -1,36 +1,66 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+//int main(void) {
+//
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	map<int, int> sCard;
+//
+//	int len, len2;
+//
+//	cin >> len;
+//	
+//	for (int i = 0; i < len; ++i) {
+//		int n;
+//		cin >> n;
+//		sCard[n]++;
+//	}
+//
+//	cin >> len2;
+//
+//	for (int i = 0; i < len2; ++i) {
+//		int n;
+//		cin >> n;
+//		if (sCard.find(n) != sCard.end()) {
+//			cout << sCard.find(n)->second << " ";
+//		}
+//		else {
+//			cout << "0 ";
+//		}
+//	}
+//}
+
 int main(void) {
+
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	map<int, int> sCard;
-
-	int len, len2;
+	int len;
+	int len2;
+	vector<int> v;
+	vector<int> vCard;
 
 	cin >> len;
-	
+
+	int card;
 	for (int i = 0; i < len; ++i) {
-		int n;
-		cin >> n;
-		sCard[n]++;
+		cin >> card;
+		v.push_back(card);
 	}
 
+	sort(v.begin(), v.end());
 	cin >> len2;
-
 	for (int i = 0; i < len2; ++i) {
-		int n;
-		cin >> n;
-		if (sCard.find(n) != sCard.end()) {
-			cout << sCard.find(n)->second << " ";
-		}
-		else {
-			cout << "0 ";
-		}
+		cin >> card;
+		cout << upper_bound(v.begin(), v.end(), card) - lower_bound(v.begin(), v.end(), card) << " ";
 	}
 }
