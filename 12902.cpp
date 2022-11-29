b25e7f378e181435a3fbd505f504ee7c83b9215a
Minkyu Kwak <lanilani7661@gmail.com>
2022-11-29 23:08:07 +0900

    [BaekJoon] solved 2133

---
+++ 12902.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    long s[2501] = { 0 };
+
+    int width = n; 
+    int length = 3;
+
+    s[0] = 0;
+    s[1] = 3;
+    s[2] = 11;
+
+    if ((width * length) % 2 != 0) {
+        return 0;
+    }
+
+    for (int i = 3; i <= n; ++i) {
+        s[i] = (((4 * s[i - 1]) % 1000000007) - ((s[i - 2]) % 1000000007)) % 1000000007;
+    }
+
+    answer = s[n / 2];
+
+
+    return answer;
+}
+
+int main(void) {
+
+    int n;
+    cin >> n;
+    cout << solution(n);
+
+}
