0028a4366f6b685f32c0fc07c3a7ac5ff9a1c693
Minkyu Kwak <lanilani7661@gmail.com>
2022-12-06 15:28:20 +0900

    solved B_1269

--- B_1269.cpp
+++ B_1269.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 int main(void) {
 
-    map<int, int> A;
-    map<int, int> B;
+    set<int> A;
+    set<int> B;
 
     int a = 0;
     int b = 0;
@@ -18,24 +18,24 @@ int main(void) {
     int num;
     for (int i = 0; i < a; ++i) {
         cin >> num;
-        A[i] = num;
+        A.insert(num);
     }
 
     for (int i = 0; i < b; ++i) {
         cin >> num;
-        B[i] = num;
+        B.insert(num);
     }
 
     int temp = a;
 
-    for (int i = 0; i < b; ++i) {
-        if (A.find(B[i]) != A.end()) {
+    for (auto iter : B) {
+        if (A.find(iter) != A.end()) {
             --a;
         }
     }
 
-    for (int i = 0; i < temp; ++i) {
-        if (B.find(A[i]) != B.end()) {
+    for (auto iter : A) {
+        if (B.find(iter) != B.end()) {
             --b;
         }
     }
@@ -43,3 +43,60 @@ int main(void) {
     cout << a + b;
 
 }
+
+//int main(void) {
+//
+//    map<int, int> A;
+//    map<int, int> B;
+//
+//    int a = 0;
+//    int b = 0;
+//    
+//    cin >> a >> b;
+//
+//    int num;
+//    for (int i = 0; i < a; ++i) {
+//        cin >> num;
+//        A[i] = num;
+//    }
+//
+//    for (int i = 0; i < b; ++i) {
+//        cin >> num;
+//        B[i] = num;
+//    }
+//
+//    int temp = a;
+//
+//    cout << "B" << endl;
+//
+//    for (auto iter : B) {
+//        if (A.find(iter.second) != A.end()) {
+//            cout << "교집합 : " << iter.second << endl;
+//            --a;
+//        }
+//    }
+//
+//    cout << endl << "A" << endl;
+//
+//    for (auto iter : A) {
+//        if (B.find(iter.second) != B.end()) {
+//            cout << "교집합 : " << iter.second << endl;
+//            --b;
+//        }
+//    }
+//
+//    //for (int i = 0; i < b; ++i) {
+//    //    if (A.find(B[i]) != A.end()) {
+//    //        --a;
+//    //    }
+//    //}
+//
+//    //for (int i = 0; i < temp; ++i) {
+//    //    if (B.find(A[i]) != B.end()) {
+//    //        --b;
+//    //    }
+//    //}
+//
+//    cout << a + b;
+//
+//}
